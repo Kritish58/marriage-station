3f42c32900d2dee4e96ec7740e0e84975a8eb6b0
spydermyaan <spydermyaan@gmail.com>
2022-02-16 18:53:11 +0545

    feat : disabled right click context menu

--- src/index.js
+++ src/index.js
@@ -5,15 +5,11 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { LogInPage } from "./pages/users/login";
-// import { FamilyInfo } from "./pages/users/complete_profile/family";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-      {/* <LogInPage /> */}
-      {/* <FamilyInfo /> */}
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
