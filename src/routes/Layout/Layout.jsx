0cb597bdbd240356f7051017df55296c78a924a0
Kritish <kritish.bhattarai@amniltech.com>
2022-04-02 21:20:32 +0545

    style: home page complete

--- src/routes/Layout/Layout.jsx
+++ src/routes/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import cx from 'classnames';
 import { Nav, Sidenav } from 'rsuite';
 import Hamburger from '../../icons/Hamburger';
@@ -13,72 +13,95 @@ import Mail from '../../icons/Mail';
 import Money from '../../icons/Upgrade';
 import Cog from '../../icons/Cog';
 import Logout from '../../icons/Logout';
+import useGetBreakpoint from '../../hooks/useGetBreakpoint';
+import { useNavigate } from 'react-router-dom';
+import { logout } from '../../redux/reducers';
 
 const Layout = ({ children }) => {
    const { user } = useSelector((state) => state.authState);
    const [expanded, setExpanded] = useState(true);
    const [activeKey, setActiveKey] = useState('1');
 
+   const breakpoint = useGetBreakpoint();
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
+
+   const handleLogout = () => {
+      dispatch(logout());
+      navigate('/', { replace: true });
+   };
+
    return (
       <div className="d-flex user__main">
          <div className={styles['layout-content']}>
-            <div className={cx(styles.sidebar, expanded && styles['sidebar-expanded'])}>
-               {/* <Sidebar /> */}
-
-               <div className={styles['rsuite-sidenav']}>
-                  <Sidenav
-                     appearance="inverse"
-                     expanded={expanded}
-                     defaultOpenKeys={['3', '4']}
-                     activeKey={activeKey}
-                     onSelect={setActiveKey}>
-                     <Sidenav.Body>
-                        <Nav>
-                           <Nav.Item className={styles['nav-item']} eventKey="1" icon={<Home />}>
-                              {!!expanded && <span>Home</span>}
-                           </Nav.Item>
-                           <Nav.Item className={styles['nav-item']} eventKey="2" icon={<User />}>
-                              {!!expanded && <span>Profile</span>}
-                           </Nav.Item>
-                           <Nav.Item className={styles['nav-item']} eventKey="3" icon={<Matches />}>
-                              {!!expanded && <span>Matches</span>}
-                           </Nav.Item>
-                           <Nav.Item className={styles['nav-item']} eventKey="4" icon={<Search />}>
-                              {!!expanded && <span>Search</span>}
-                           </Nav.Item>
-                           <Nav.Item className={styles['nav-item']} eventKey="5" icon={<Mail />}>
-                              {!!expanded && <span>Mailbox</span>}
-                           </Nav.Item>
-                           <Nav.Item className={styles['nav-item']} eventKey="6" icon={<Money />}>
-                              {!!expanded && <span>Upgrade</span>}
-                           </Nav.Item>
-                           <Nav.Item className={styles['nav-item']} eventKey="7" icon={<Cog />}>
-                              {!!expanded && <span>Settings</span>}
-                           </Nav.Item>
-                        </Nav>
-                     </Sidenav.Body>
-                  </Sidenav>
-               </div>
-
-               <div className={styles.logout}>
-                  <Logout />
-                  {!!expanded && <span>Logout</span>}
-               </div>
-            </div>
-
-            <div className={cx(styles.topbar, expanded && styles['topbar-expanded'])}>
+            <div
+               className={cx(
+                  styles.topbar,
+                  expanded && styles['topbar-expanded'],
+                  (breakpoint === 'sm' || breakpoint === 'md') && styles['topbar-smallScreen']
+               )}>
                <Hamburger
                   className={styles.menu}
                   onClick={() => {
-                     setExpanded((prev) => !prev);
+                     breakpoint !== 'sm' && breakpoint !== 'md' && setExpanded((prev) => !prev);
                   }}
                />
                <h5 className={styles['topbar-title']}>Hello, {user.firstName}</h5>
             </div>
+
+            {(breakpoint === 'xl' || breakpoint === 'xxl' || breakpoint === 'lg') && (
+               <div className={cx(styles.sidebar, expanded && styles['sidebar-expanded'])}>
+                  {/* <Sidebar /> */}
+
+                  <div className={styles['rsuite-sidenav']}>
+                     <Sidenav
+                        appearance="inverse"
+                        expanded={expanded}
+                        defaultOpenKeys={['3', '4']}
+                        activeKey={activeKey}
+                        onSelect={setActiveKey}>
+                        <Sidenav.Body>
+                           <Nav>
+                              <Nav.Item className={styles['nav-item']} eventKey="1" icon={<Home />}>
+                                 {!!expanded && <span>Home</span>}
+                              </Nav.Item>
+                              <Nav.Item className={styles['nav-item']} eventKey="2" icon={<User />}>
+                                 {!!expanded && <span>Profile</span>}
+                              </Nav.Item>
+                              <Nav.Item className={styles['nav-item']} eventKey="3" icon={<Matches />}>
+                                 {!!expanded && <span>Matches</span>}
+                              </Nav.Item>
+                              <Nav.Item className={styles['nav-item']} eventKey="4" icon={<Search />}>
+                                 {!!expanded && <span>Search</span>}
+                              </Nav.Item>
+                              <Nav.Item className={styles['nav-item']} eventKey="5" icon={<Mail />}>
+                                 {!!expanded && <span>Mailbox</span>}
+                              </Nav.Item>
+                              <Nav.Item className={styles['nav-item']} eventKey="6" icon={<Money />}>
+                                 {!!expanded && <span>Upgrade</span>}
+                              </Nav.Item>
+                              <Nav.Item className={styles['nav-item']} eventKey="7" icon={<Cog />}>
+                                 {!!expanded && <span>Settings</span>}
+                              </Nav.Item>
+                           </Nav>
+                        </Sidenav.Body>
+                     </Sidenav>
+                  </div>
+
+                  <div className={styles.logout} onClick={handleLogout}>
+                     <Logout />
+                     {!!expanded && <span>Logout</span>}
+                  </div>
+               </div>
+            )}
          </div>
 
          <aside
-            className={cx(styles['main-content'], expanded && styles['main-content-expanded'])}
+            className={cx(
+               styles['main-content'],
+               expanded && styles['main-content-expanded'],
+               (breakpoint === 'sm' || breakpoint === 'md') && styles['main-content-smallScreen']
+            )}
             style={{ flexGrow: '1' }}>
             {children}
          </aside>
